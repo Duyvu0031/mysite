c25e4e82c1229aa0af36c98b179ae39e4a6f4b40
Duyvu0031 <nguyenvutungduy@gmail.com>
2025-01-06 15:16:51 +0700

    sua loi dang nhap that bai

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,23 +3,33 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    unless User.exists?(email_address: 'admin@example.com')
+      begin
+        User.create!(email_address: 'admin@example.com', password: 'duy0031')
+      rescue ActiveRecord::RecordInvalid => e
+        logger.error "Failed to create admin user: #{e.message}"
+      end
+    end
   end
 
   def create
     # Nếu là tài khoản chính
     if params[:email_address] == "duy0031@gmail.com" && params[:password] == "duy0031"
-      user = User.find_or_initialize_by(email_address: "duy0031@gmail.com")
-      user.update!(password: "duy0031") unless user.persisted?
+      user = User.find_or_create_by(email_address: "duy0031@gmail.com") do |user|
+        user.password = "duy0031"
+      end
       start_new_session_for user
       redirect_to after_authentication_url
     # Xác thực tài khoản thông thường
-    elsif user = User.authenticate_by(params.permit(:email_address, :password))
+    elsif user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
       start_new_session_for user
       redirect_to after_authentication_url
     else
       redirect_to new_session_path, alert: "Hãy thử một địa chỉ email hoặc mật khẩu khác."
     end
   end
+  
+  
 
   def destroy
     terminate_session
